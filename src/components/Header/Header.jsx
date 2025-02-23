4692910182b60998ba1f173c1212d022b736b6bd
DRoyyy <dipta.contact@gmail.com>
2025-02-23 05:37:46 +0100

    Add base structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import logo from "../../assets/images/logo.png";
+import bgShadow from "../../assets/images/bg-shadow.png";
+import banner from "../../assets/images/banner-main.png";
+import PropTypes from 'prop-types';
+
+const Header = ({addFreeCredit, credit, creditClaimed}) => {
+  
+
+  return (
+    <div className="max-w-screen-2xl mx-auto m-8">
+      <nav className="flex items-center justify-between">
+        <img src={logo} alt="logo" />
+        <div className="flex items-center justify-center gap-4 text-[#131313B3]">
+          <ul className="flex gap-4">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Teams</li>
+            <li className="cursor-pointer">Fixtures</li>
+            <li className="cursor-pointer">About</li>
+          </ul>
+          <button className="border-2 rounded-lg px-2 py-1 font-semibold">
+            <span>{credit}</span> Coin
+          </button>
+        </div>
+      </nav>
+      <div
+        className="bg-[#131313] bg-cover rounded-xl flex flex-col items-center py-20 my-8 gap-y-8"
+        style={{ backgroundImage: `url(${bgShadow})` }}
+      >
+        <img src={banner} alt="banner" />
+        <h2 className="text-4xl text-white font-bold">
+          Assemble Your Ultimate Dream 11 Cricket Team
+        </h2>
+        <h3 className="text-2xl text-white font-medium">
+          Beyond Boundaries Beyond Limits
+        </h3>
+        <button
+          onClick={addFreeCredit}
+          disabled={creditClaimed}
+          className={`px-4 py-3 rounded-lg font-semibold ${
+            creditClaimed ? "bg-gray-400 cursor-not-allowed" : "bg-yellow-400"
+          }`}
+        >
+          Claim Free Credit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Header.propTypes = {
+    addFreeCredit: PropTypes.func,
+    credit: PropTypes.number,
+    creditClaimed: PropTypes.bool
+}
+
+export default Header;
---
+++ src/components/SelectedPlayersList/SelectedPlayersList.jsx
@@ -0,0 +1,22 @@
+import SelectedPlayer from "../SelectedPlayer/SelectedPlayer";
+import PropTypes from 'prop-types';
+
+const SelectedPlayersList = ({selectedPlayersList, handleDeletePlayer, handleShowAvailable}) => {
+    return (
+        <div className="max-w-screen-2xl mx-auto m-3 flex flex-col gap-5 pb-60">
+            <h2 className='text-3xl font-bold'>Selected Players ({selectedPlayersList.length}/11)</h2>
+            {
+                selectedPlayersList.map((player, idx) => (<SelectedPlayer key={idx} player={player} handleDeletePlayer={handleDeletePlayer}></SelectedPlayer>))
+            }
+            <button onClick={()=> handleShowAvailable(true)} className={"bg-yellow-400 w-[180px] font-semibold px-5 py-3 border rounded-lg"}>Add More Player</button>
+        </div>
+    );
+};
+
+SelectedPlayersList.propTypes = {
+    selectedPlayersList: PropTypes.object.isRequired,
+    handleDeletePlayer: PropTypes.func,
+    handleShowAvailable: PropTypes.func    
+}
+
+export default SelectedPlayersList;
